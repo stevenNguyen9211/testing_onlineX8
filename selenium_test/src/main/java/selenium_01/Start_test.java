fd17816b1f13e20790bd0052e40b2fd98337eb07
steven.nguyen9211@gmail.com <steven.nguyen9211@gmail.com>
2018-09-01 23:38:51 +0700

    Add new project

---
+++ selenium_test/src/main/java/selenium_01/Start_test.java
@@ -0,0 +1,23 @@
+package selenium_01;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Start_test {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "src/main/resources/data/browser_driver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com.vn");
+		Thread.sleep(3000);
+		String getTitlePage = driver.getTitle();
+		System.out.println(getTitlePage);
+		driver.quit();
+	}
+
+}
