f34dc0f9d9fae29643722b53517c44b45361b29f
NguyenKhoi <khoinguyenkq937@gmail.com>
2018-09-02 00:51:05 +0700

    Update with new name NguyenKhoi

--- selenium_test/src/main/java/selenium_01/Start_test.java
+++ selenium_test/src/main/java/selenium_01/Start_test.java
@@ -14,7 +14,7 @@ public class Start_test {
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get("https://www.accorhotels.com/");
-		Thread.sleep(10000);
+		Thread.sleep(8000);
 		String getTitlePage = driver.getTitle();
 		System.out.println(getTitlePage);
 		driver.quit();
